bd7d9ebdfaefe60ef60e7b90c8d3d8959638df55
SRyu9 <61402640+SRyu9@users.noreply.github.com>
2022-03-12 23:05:42 +0900

    マスタ・トランテーブルをモデルに作成

--- ShareDiary/ShareDiaryProject/user/models.py
+++ ShareDiary/ShareDiaryProject/user/models.py
@@ -1,9 +1,30 @@
 from django.db import models
 
 # Create your models here. -- データベースを操作するときに利用するファイル
-class UserModel(models.Model):  #クラス継承
-    user_id = models.CharField(max_length=100)
+
+# ユーザーマスタ
+class M_USER(models.Model):  #クラス継承
+    user_id = models.CharField(max_length=100, primary_key=True)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    insert_datetime = models.DateTimeField(auto_now_add=True)
+    update_datetime = models.DateTimeField(auto_now_add=True)
 
+# ユーザーの友達マスタ
+class M_USER_FRIENDS(models.Model):
+    user_id = models.CharField(max_length=100, primary_key=True)
+    frind_user_id = models.CharField(max_length=100, primary_key=True)
+    share_start_datetime = models.DateTimeField(auto_now_add=True)
+    insert_datetime = models.DateTimeField(auto_now_add=True)
+    update_datetime = models.DateTimeField(auto_now_add=True)
 
+# 日記トラン
+class T_DIARY(models.Model):
+    diary_id = models.CharField(max_length=100)
+    diary_title = models.CharField(max_length=100)
+    diary_text = models.CharField(max_length=250)
+    post_user_id = models.CharField(max_length=100)
+    post_datetime = models.DateTimeField(auto_now_add=True)
+    insert_datetime = models.DateTimeField(auto_now_add=True)
+    update_datetime = models.DateTimeField(auto_now_add=True)
+    
--- ShareDiary/ShareDiaryProject/user/views.py
+++ ShareDiary/ShareDiaryProject/user/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import TemplateView 
+from django.views.generic import TemplateView
 
 # Create your views here.
 
